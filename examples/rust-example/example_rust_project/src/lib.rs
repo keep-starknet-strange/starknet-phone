e77101eef9128e9a67e95c506921afd26c4c31ea
Thomas <thomas@pop-os.cable.rcn.com>
2024-08-06 05:32:27 -0500

    add test

--- examples/rust-example/example_rust_project/src/lib.rs
+++ examples/rust-example/example_rust_project/src/lib.rs
@@ -3,3 +3,15 @@ mod android;
 pub fn hello(input: &str) -> String {
     format!("Hello, {}", input)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input: &str = "Test";
+        let result = hello(input);
+        assert_eq!(result, "Hello, Test")
+    }
+}
