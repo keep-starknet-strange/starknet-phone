fb5d1c28f0d0119e9c9fe2914acb7c0d8a6c8a51
Thomas Butler <58192340+trbutler4@users.noreply.github.com>
2024-08-06 07:14:58 -0500

    Rust example app (#30)
    * progress
    
    * progress?
    
    * tweaks
    
    * add test
    
    * removing target dir and some tweaks
    
    * add target to ignored files
    
    * clearing old files
    
    * new directory structure
    
    * working log printed
    
    * created makefile
    
    * adding readme
    
    ---------
    
    Co-authored-by: Thomas <thomas@pop-os.cable.rcn.com>

---
+++ examples/rust-example/android/app/src/main/java/com/example/rust_example/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.rust_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val result = RustLib.hello("TEST")
+        println(result)
+    }
+}
