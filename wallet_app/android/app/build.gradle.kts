ff85a2781fa4573b10d0df6b39ce7c5b486c2bd3
Sajal bansal <77548932+sajalbnl@users.noreply.github.com>
2024-08-13 09:56:32 +0530

    Wallet Ui (#39)
    * Wallet Ui
    
    * Updated dependencies
    
    ---------
    
    Co-authored-by: Sajal Bansal <77548932+sajal-madmax@users.noreply.github.com>

--- wallet_app/android/app/build.gradle.kts
+++ wallet_app/android/app/build.gradle.kts
@@ -7,6 +7,11 @@ android {
     namespace = "com.example.walletapp"
     compileSdk = 34
 
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
     defaultConfig {
         applicationId = "com.example.walletapp"
         minSdk = 24
@@ -33,6 +38,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
@@ -45,4 +58,47 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    // Material Design 3
+    implementation(libs.androidx.material3)
+    // or Material Design 2
+    implementation(libs.androidx.material)
+
+    // Retrofit for network requests
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    //dagger
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.fragment)
+    implementation (libs.androidx.hilt.navigation.compose.v100alpha03)
+
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+
+    // Optional - Integration with activities
+    implementation(libs.androidx.activity.compose)
+    // Optional - Integration with ViewModels
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    // Optional - Integration with LiveData
+    implementation(libs.androidx.runtime.livedata)
+
+    implementation(libs.androidx.navigation.compose)
+
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(kotlin("script-runtime"))
 }
