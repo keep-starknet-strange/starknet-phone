66ae86671308312d4a477669923bdab0578369ba
Sajal Bansal <77548932+sajal-madmax@users.noreply.github.com>
2024-09-13 00:41:43 +0530

    starknet sdk

--- wallet_app/android/build.gradle.kts
+++ wallet_app/android/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 
-    id("com.google.dagger.hilt.android") version "2.50" apply false
+    id("org.jmailen.kotlinter") version "3.15.0"
 }
