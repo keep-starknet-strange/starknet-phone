982522a48863fe5770b0593e67b46fd6fc10d90e
Thomas <trbiv@proton.me>
2024-12-21 01:10:21 -0600

    tweaking android.bp and bundling script

---
+++ aosp_bundler.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# example applications
+mkdir -p ../aosp/packages/apps/DappExample
+cp examples/DappExample/app/Android.bp ../aosp/packages/apps/DappExample/
+cp -r examples/DappExample/app/src ../aosp/packages/apps/DappExample/
+
