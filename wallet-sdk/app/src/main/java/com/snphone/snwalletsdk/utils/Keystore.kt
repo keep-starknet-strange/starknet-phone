55ef0e0cdc8b8aaa450c080afffa38ccfa95a8c1
Kishan Patel <55874508+maxslimb@users.noreply.github.com>
2025-01-07 20:17:15 -0600

    Implemented Wallet SDK (#139)
    * implemented basic functions
    
    * implemented and tested the deployAccount function
    
    * implemented and tested the deployAccount function
    
    * implemented and tested the balance function
    
    * implemented and tested the TransferFunds function
    
    * removed unused imports
    
    * updated return parameters and other fixes

---
+++ wallet-sdk/app/src/main/java/com/snphone/snwalletsdk/utils/Keystore.kt
@@ -0,0 +1,64 @@
+package com.snphone.snwalletsdk.utils
+
+import android.util.Log
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.snphone.snwalletsdk.MainActivity
+import com.snphone.snwalletsdk.SNWalletSDK
+import java.io.IOException
+import java.security.GeneralSecurityException
+
+class Keystore() {
+
+    private val tag = "Keystore"
+    private val context = MainActivity.applicationContext()
+    fun storeData(message: String) {
+        try {
+            val masterKey = MasterKey.Builder(context)
+                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                .build()
+
+            val sharedPreferences = EncryptedSharedPreferences.create(
+                context,
+                "my_encrypted_prefs",
+                masterKey,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            )
+
+            val editor = sharedPreferences.edit()
+            editor.putString("key", message)
+            editor.apply()
+
+        } catch (e: GeneralSecurityException) {
+            Log.e(tag, "Security exception while storing data: ${e.message}", e)
+        } catch (e: IOException) {
+            Log.e(tag, "I/O exception while storing data: ${e.message}", e)
+        }
+    }
+
+    fun retrieveData(): String {
+        try {
+            val masterKey = MasterKey.Builder(context)
+                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                .build()
+
+            val sharedPreferences = EncryptedSharedPreferences.create(
+                context,
+                "my_encrypted_prefs",
+                masterKey,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            )
+
+            return sharedPreferences.getString("key", "") ?: ""
+
+        } catch (e: GeneralSecurityException) {
+            Log.e(tag, "Security exception while retrieving data: ${e.message}", e)
+            return ""
+        } catch (e: IOException) {
+            Log.e(tag, "I/O exception while retrieving data: ${e.message}", e)
+            return ""
+        }
+    }
+}
