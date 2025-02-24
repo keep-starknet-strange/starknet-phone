b24cc0c5bf3bee6002fb78a226170f8ea6c4999c
Thomas Robert Butler IV <thomas@nakamotomining.ai>
2025-02-24 14:37:15 -0600

    updating dapp example

--- examples/DappExample/app/build.gradle.kts
+++ examples/DappExample/app/build.gradle.kts
@@ -50,7 +50,8 @@ android {
 }
 
 dependencies {
-
+    implementation(files("../../../wallet-sdk/walletsdk/build/outputs/aar/walletsdk-release.aar"))
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
